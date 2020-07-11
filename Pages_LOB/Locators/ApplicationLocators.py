f359c121cc9387d632c7c7f1cc7e1e4ad8c3359d
Sharath Akumalla <sharath.akumalla3110@gmail.com>
2020-07-11 19:24:27 +0530

    Added Invalid Username Xpath

--- Pages_LOB/Locators/ApplicationLocators.py
+++ Pages_LOB/Locators/ApplicationLocators.py
@@ -6,6 +6,7 @@ Loginpage = {"URL":{"URL":"https://stingray.knigrp.com/login.max?preprocess=true
             "Password":{"Name":"password"},
             "LoginButton":{"Name":"submit"},
             "HOFullQuoteLink":{"link_text":"Full Quote - HO"},
-            "SpinnerOverlay":{"id":"spinnerOverlay"}
+            "SpinnerOverlay":{"id":"spinnerOverlay"},
+            "InvalidUsername":{"Xpath":"//div[contains(text(),'Invalid Login - Your user account will be locked o')]"}
              }
 
