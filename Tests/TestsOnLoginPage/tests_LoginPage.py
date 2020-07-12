9e7e6c8556d0e8e56717e3a3b30bf4b2a23dd38c
Sharath Akumalla <sharath.akumalla3110@gmail.com>
2020-07-12 11:28:54 +0530

    Added the Lessons learnt today

--- Tests/TestsOnLoginPage/tests_LoginPage.py
+++ Tests/TestsOnLoginPage/tests_LoginPage.py
@@ -11,29 +11,23 @@ class tests_LoginPage(unittest.TestCase):
     driver = obj_GetDriverInstance.DriverInstance()
     obj_login = LoginPage(driver)
 
-    # def __init__(self):
-    #     obj_GetDriverInstance = GetDriverInstance(browser="firefox")
-    #     self.driver = obj_GetDriverInstance.DriverInstance()
-    #     self.obj_login=LoginPage(self.driver)
-
+    # Valid Credentials
     def test_TC1_ValidLogin(self):
         self.obj_login.login(userName=TD.ValidUsername,password=TD.ValidPassword)
         result=self.obj_login.verifyLoginSuceesful()
 
 
+    # Valid user Name , Invalid Password
     def TC2_InvalidLogin(self):
-        #Valid user Name , Invalid Password
         self.obj_login.login(userName=TD.ValidUsername,password=TD.InValidPassword)
         result=self.obj_login.verifyInvalidUsername()
 
-
+    # Invalid user name , Valid Password
     def TC3_InvalidLogin(self):
-        #invalid user name , Valid Password
         self.obj_login.login(userName=TD.InValidUsername,password=TD.ValidPassword)
         result = self.obj_login.verifyInvalidUsername()
 
-
+    # Invalid user name and password
     def TC4_InvalidLogin(self):
-        #invalid user name and password
         self.obj_login.login(userName=TD.InValidUsername,password=TD.InValidPassword)
         result=self.obj_login.verifyLoginFailed()
