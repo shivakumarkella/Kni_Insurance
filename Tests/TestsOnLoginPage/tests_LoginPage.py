5a41b4f6a70196d0468e2a8bfc88391c834026a2
Kella Shiva Kumar <shivakumar.kella@gmail.com>
2020-07-13 12:18:36 +0530

    added code for logger and path

--- Tests/TestsOnLoginPage/tests_LoginPage.py
+++ Tests/TestsOnLoginPage/tests_LoginPage.py
@@ -3,18 +3,22 @@ from Tests.testData import TestData as TD
 from Documents.Configfile.GetDrivers import GetDriverInstance
 import pytest
 import unittest
+from TestResults.customLogger import customLogger
 
 @pytest.mark.usefixtures("oneTimeSetUp")
 class tests_LoginPage(unittest.TestCase):
 
     @pytest.fixture(autouse=True)
     def thisclassSetup(self):
+        self.log = customLogger()
         self.obj_login = LoginPage(self.driver)
 
-    @pytest.mark.run(order=2)
-    def testCase1_ValidLogin(self):
-        self.obj_login.login(userName=TD.ValidUsername,password=TD.ValidPassword)
-        result=self.obj_login.verifyLoginSuceesful()
+
+    # @pytest.mark.run(order=2)
+    # def testCase1_ValidLogin(self):
+    #     self.obj_login.login(userName=TD.ValidUsername,password=TD.ValidPassword)
+    #     result=self.obj_login.verifyLoginSuceesful()
+
 
     # @pytest.mark.run(order=3)
     # def testCase2_InvalidLogin(self):
@@ -22,11 +26,11 @@ class tests_LoginPage(unittest.TestCase):
     #     self.obj_login.login(userName=TD.ValidUsername,password=TD.InValidPassword)
     #     result=self.obj_login.verifyInvalidUsername()
 
-    @pytest.mark.run(order=4)
-    def testCase3_InvalidLogin(self):
-        #invalid user name , Valid Password
-        self.obj_login.login(userName=TD.InValidUsername,password=TD.ValidPassword)
-        result = self.obj_login.verifyInvalidUsername()
+    # @pytest.mark.run(order=4)
+    # def testCase3_InvalidLogin(self):
+    #     #invalid user name , Valid Password
+    #     self.obj_login.login(userName=TD.InValidUsername,password=TD.ValidPassword)
+    #     result = self.obj_login.verifyInvalidUsername()
 
     @pytest.mark.run(order=1)
     def testCase4_InvalidLogin(self):
--- Tests/testData/TestData.py
+++ Tests/testData/TestData.py
@@ -7,3 +7,6 @@ InValidPassword = "8592212983"
 InValidUsername = "brxyz"
 
 location="C:\\Users\\Sharat\\Workspace_python\\Kni_Insurance\\TestResults\\Screenshots"
+
+AutomationFileName = 'automation'
+pathToSaveAutomationLog = "TestResults//DocsScreenShotsLogFiles//"
