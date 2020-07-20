239c68de4fff558263991a1395a2dc327d51d650
Kella Shiva Kumar <shivakumar.kella@gmail.com>
2020-07-20 23:46:15 +0530

    fixed the property zip

--- Tests/testData/TestData.py
+++ Tests/testData/TestData.py
@@ -1,12 +1,12 @@
 import pandas
 import datetime as dt
 
-excelRowNumber=162
+excelRowNumber=244
 
 rowNumber=excelRowNumber-1
 dataRow=rowNumber-1
 
-path='D:\\sharath Data\\HO_UserData.xlsx'
+path='C:\\Users\\kella\workspace_python\\HO_UserData.xlsx'
 data=pandas.read_excel(path, sheet_name='APLUS Claims Data',nrows=rowNumber, usecols=['LAST NAME', 'FIRST NAME','INSURED SSN',
                                                                                       'INSURED DOB','INSURED GENDER',
                                                                                       'STREET NUMBER','STREET NAME','STREET TYPE',
@@ -19,24 +19,22 @@ data=data.to_dict(orient='records')
 LastName=list(data[dataRow].items())[0][1]
 FirstName = list(data[dataRow].items())[1][1]
 SSN = int(list(data[dataRow].items())[2][1])
-# DOB= dt.datetime.strptime(str(int(list(data[dataRow].items())[3][1])),'%m%d%Y').date()
+# DOB1= dt.datetime.strptime(str(int(list(data[dataRow].items())[3][1])),'%m%d%Y').date()
+# DOB=dt.datetime.strptime(DOB1,'%m/%d/%Y')
 selectgender = list(data[dataRow].items())[4][1]
 
 StreetNumber = list(data[dataRow].items())[5][1]
 StreetName = list(data[dataRow].items())[6][1]
 StreetBluff = list(data[dataRow].items())[7][1]
 propertyAddress1=str(StreetNumber)+' '+str(StreetName)+' '+str(StreetBluff)
-
-# NumberaApt = list(data[dataRow].items())[8][1]
-# if str(NumberaApt)=='nan':
-#     aptNumber=' '
-# else:
-#     aptNumber=NumberaApt
-
-
+NumberaApt = list(data[dataRow].items())[8][1]
+if str(NumberaApt)=='nan':
+    aptNumber=' '
+else:
+    aptNumber=NumberaApt
 propertyCity = list(data[dataRow].items())[9][1]
 propertyState = list(data[dataRow].items())[10][1]
-# propertyZip = list(data[dataRow].items())[11][1]
+propertyZip = str(int(list(data[dataRow].items())[11][1]))
 policyType = list(data[dataRow].items())[12][1]
 policyNumber = list(data[dataRow].items())[13][1]
 accountNumber = list(data[dataRow].items())[14][1]
@@ -54,16 +52,13 @@ ValidUsername = "xlsol"
 ValidPassword = "1234"
 InValidPassword = "8592212983"
 InValidUsername = "brxyz"
-# FirstName="Test"
-# LastName="Test"
-# SSN="123321115"
+
 DOB="10/31/1990"
 Email="test@test.com"
-# propertyAddress1="190 Stump Bluff	Rd"
+
 propertyAddress2="     "
-# propertyCity="Bowling Green"
-# propertyState="KY"
-propertyZip="42103"
+
+
 YearsAtCurrentAddress ="5"
 selectvalueforagency="486"
 selectCounty ="67"
@@ -91,7 +86,6 @@ selectvalueforWallcondition="1"
 selectvalueforFoundationcondition="1"
 
 # Values to be selected Coverages Page
-
 selctvalueforCoverageE="100000"
 selectvalueforCoverageF="2000"
 selctvalueforPolicyDeductible="1000"
